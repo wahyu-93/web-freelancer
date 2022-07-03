95e38f61b77baa5acce9a40ece3c98cfb5c0a945
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2022-07-03 18:36:47 +0800

    add sofdeletes dan protetected  tiap model2 [done]

--- app/Models/DetailUser.php
+++ app/Models/DetailUser.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DetailUser extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $dates = [
+        'deleted_at',
+        'created_at',
+        'updated_at'
+    ];
 }
