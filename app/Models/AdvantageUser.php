c7cf7f777fc6ceb331180b99120e684bd6f2b5e6
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2022-07-03 18:35:21 +0800

    add softdeletes dan protected  tiap model [done]

--- app/Models/AdvantageUser.php
+++ app/Models/AdvantageUser.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class AdvantageUser extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $dates = [
+        'deleted_at',
+        'created_at',
+        'updated_at'
+    ];
 }
