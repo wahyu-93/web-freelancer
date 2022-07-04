6b17af19405f7fb8f33a4b69249fdf8c01eedf2b
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2022-07-04 22:46:13 +0800

    slicing include dan layouts [done]

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_'. '-'. app()->getLocale()) }}">
+    <head>
+        @include('includes.landing._meta')
+
+        <title>@yield('name') | SERV</title>
+        
+        @stack('before-style')
+
+        @include('includes.landing._style')
+
+        @stack('after-style')
+    </head>
+    <body class="antialiased">
+        <div class="relative">
+            @include('includes.landing._header')
+
+                {{-- @include('sweetalert::alert') --}}
+
+                @yield('content')
+
+            @include('includes.landing._footer')
+
+            @stack('before-script')
+
+            @include('includes.landing._script')
+    
+            @stack('after-script')
+
+            {{-- modeal --}}
+            @include('components.modal.login')
+            @include('components.modal.register')
+            @include('components.modal.register-success')
+        </div>
+    </body>
+</html>
