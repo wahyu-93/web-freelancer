15d0d9a6b7d4132599a0e0c795f1b3993cb21269
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2022-07-08 14:18:37 +0800

    slicing service [done]

--- resources/views/pages/dashboard/service/create.blade.php
+++ resources/views/pages/dashboard/service/create.blade.php
@@ -15,23 +15,23 @@
                     </p>
                 </div>
             </div>    
+            
+            <!-- breadcrumb -->
+            <nav class="mx-10 mt-8 text-sm" aria-label="Breadcrumb">
+               <ol class="inline-flex p-0 list-none">
+                   <li class="flex items-center">
+                       <a href="{{ route('member.service.index') }}" class="text-gray-400">My Services</a>
+                       <svg class="w-3 h-3 mx-3 text-gray-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                           <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
+                       </svg>
+                   </li>
+                   <li class="flex items-center">
+                       <p class="font-medium">Add Your Service</p>
+                   </li>
+               </ol>
+           </nav>
         </div>
        
-        <!-- breadcrumb -->
-        <nav class="mx-8 mt-8 text-sm" aria-label="Breadcrumb">
-           <ol class="inline-flex p-0 list-none">
-               <li class="flex items-center">
-                   <a href="{{ route('member.service.index') }}" class="text-gray-400">My Services</a>
-                   <svg class="w-3 h-3 mx-3 text-gray-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                       <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
-                   </svg>
-               </li>
-               <li class="flex items-center">
-                   <p class="font-medium">Add Your Service</p>
-               </li>
-           </ol>
-       </nav>
-        
         <section class="container px-6 mx-auto mt-5">
             <div class="grid gap-5 md:grid-cols-12">
                 <main class="col-span-12 p-4 md:pt-0">
@@ -126,9 +126,9 @@
                                 </div>
 
                                 <div class="px-4 py-3 text-right sm:px-6">
-                                    <button type="submit" class="inline-flex justify-center px-4 py-2 mr-4 text-sm font-medium text-gray-700 bg-white border border-gray-600 rounded-lg shadow-sm hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300">
+                                    <a href="{{ route('member.service.index') }}" class="inline-flex justify-center px-4 py-2 mr-4 text-sm font-medium text-gray-700 bg-white border border-gray-600 rounded-lg shadow-sm hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300">
                                         Cancel
-                                    </button>
+                                    </a>
 
                                     <button type="submit" class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-green-600 border border-transparent rounded-lg shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
                                         Create Service
